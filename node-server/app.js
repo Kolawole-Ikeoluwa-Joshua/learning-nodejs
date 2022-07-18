62cb614b3decdd92777b228c14b7c1120619864c
Kolawole Ikeoluwa Joshua <kolawoleikeoluwa2018@gmail.com>
2022-07-18 16:20:17 +0100

    add nodemon monitor script

--- node-server/app.js
+++ node-server/app.js
@@ -4,8 +4,9 @@ const http = require('http');
 // importing custom module
 const routes = require('./routes');
 
+
 // creating http response server with an anonymous func
 
 const server = http.createServer(routes);
 
-server.listen(3000);
+server.listen(3500);
