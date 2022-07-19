f903a29f4633e003f0ab6d8a3f1a0ca861c9017b
Kolawole Ikeoluwa Joshua <kolawoleikeoluwa2018@gmail.com>
2022-07-19 16:22:22 +0100

    create express application to implement middleware

--- node-server-express/app.js
+++ node-server-express/app.js
@@ -4,12 +4,27 @@ const http = require('http');
 // importing 3rd party Nodejs modules
 const express = require('express');
 
+
+// express function used to create an express application
 const app = express();
 
+
+// use created express application to implement middleware
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Allows the request to continue to the next middleware in line
+
+});
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+
+});
 
 
 // creating http response server
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(3500);
